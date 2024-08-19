1a021ae5cc044ce7472c54c7b8de011d5a09e652
uniend5959 <uniend5959@gmail.com>
2024-08-19 23:57:32 +0900

    '가짜데이터'

---
+++ src/_Mock/pen.js
@@ -0,0 +1,233 @@
+
+
+  const PEN = {
+    homeProduct:[
+      {
+        id:1,
+        name: '프러스펜 3000 프라임',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_01.jpg'
+      },
+      {
+        id:2,
+        name: '153 리스펙트 리파인',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_02.jpg'
+      },
+      {
+        id:3,
+        name: '153 아이디 만년필 ',
+        type: '만년필',
+        imgURL: './image/img_product_list_03.jpg'
+      },
+      {
+        id:4,
+        name: '153 네오 만년필 EF',
+        type: '만년필',
+        imgURL: './image/img_product_list_04.jpg'
+      },
+      {
+        id:5,
+        name: '라인 만년필',
+        type: '만년필',
+        imgURL: './image/img_product_list_05.jpg'
+      },
+      {
+        id:6,
+        name: '153 네이처',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_06.jpg'
+      },
+      {
+        id:7,
+        name: '153 블라썸',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_07.jpg'
+      },
+      {
+        id:8,
+        name: '153 네오 만년필 F',
+        type: '만년필',
+        imgURL: './image/img_product_list_08.jpg'
+      },
+      {
+        id:9,
+        name: '153 블랙 & 화이트',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_09.jpg'
+      },
+      {
+        id:10,
+        name: '153 골드',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_10.jpg'
+      },
+      {
+        id:11,
+        name: '153 네오 밀키',
+        type: '프리미엄 펜',
+        imgURL:'./image/img_product_list_11.jpg'
+      },
+      {
+        id:12,
+        name: '153 아이디',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_12.jpg'
+      },
+      {
+        id:13,
+        name: '60주년 프러스펜 3000 데스크펜 스페셜 에디션',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_13.jpg'
+      },
+      {
+        id:14,
+        name: '153 피셔맨',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_14.jpg'
+      },
+      {
+        id:15,
+        name: '153 네오 빈티지',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_15.jpg'
+      },
+      {
+        id:16,
+        name: '153 아이디 샤프',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_16.jpg'
+      },
+      {
+        id:17,
+        name: '153 네오',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_17.jpg'
+      },
+      {
+        id:18,
+        name: '153 리스펙트',
+        type: '프리미엄 펜',
+        imgURL: './image/img_product_list_18.jpg'
+      },
+    ],
+    tab:{
+      Premium:[
+        {
+          id:1,
+          name: '프러스펜 3000 프라임',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_01.jpg'
+        },
+        {
+          id:2,
+          name: '153 리스펙트 리파인',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_02.jpg'
+        },
+        {
+          id:6,
+          name: '153 네이처',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_06.jpg'
+        },
+        {
+          id:7,
+          name: '153 블라썸',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_07.jpg'
+        },
+        {
+          id:9,
+          name: '153 블랙 & 화이트',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_09.jpg'
+        },
+        {
+          id:10,
+          name: '153 골드',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_10.jpg'
+        },
+        {
+          id:11,
+          name: '153 네오 밀키',
+          type: '프리미엄 펜',
+          imgURL:'./image/img_product_list_11.jpg'
+        },
+        {
+          id:12,
+          name: '153 아이디',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_12.jpg'
+        },
+        {
+          id:13,
+          name: '60주년 프러스펜 3000 데스크펜 스페셜 에디션',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_13.jpg'
+        },
+        {
+          id:14,
+          name: '153 피셔맨',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_14.jpg'
+        },
+        {
+          id:15,
+          name: '153 네오 빈티지',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_15.jpg'
+        },
+        {
+          id:17,
+          name: '153 네오',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_17.jpg'
+        },
+        {
+          id:18,
+          name: '153 리스펙트',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_18.jpg'
+        },
+      ],
+      pen:[
+        {
+          id:3,
+          name: '153 아이디 만년필 ',
+          type: '만년필',
+          imgURL: './image/img_product_list_03.jpg'
+        },
+        {
+          id:4,
+          name: '153 네오 만년필 EF',
+          type: '만년필',
+          imgURL: './image/img_product_list_04.jpg'
+        },
+        {
+          id:5,
+          name: '라인 만년필',
+          type: '만년필',
+          imgURL: './image/img_product_list_05.jpg'
+        },  
+        {
+          id: 19,
+          name: '153 네오 만년필',
+          type: '만년필',
+          imgURL: './image/img_product_list_08.jpg'
+        },
+      ],
+      pencil:[
+        {
+          id:16,
+          name: '153 아이디 샤프',
+          type: '프리미엄 펜',
+          imgURL: './image/img_product_list_16.jpg'
+        },
+      ]
+    }
+  }
+
+
+  export default PEN;
