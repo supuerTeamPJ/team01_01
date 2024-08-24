6c72bbb6bd2281da2874308797741897f8fbb75d
uniend5959 <uniend5959@gmail.com>
2024-08-24 13:05:08 +0900

    '전역상태관리등록'

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,13 +1,20 @@
 
-import React from "react"
 import PageTop from "./listpage/pagetop"
 import Content from "./listpage/content/content"
 import Pagenation from "./listpage/pagenation"
 import styled from "styled-components"
+import { useContext, useEffect } from "react"
+import { ProductStore } from "../../context/productcontext"
 
 const ProductList = () =>{
+    //저장소 불러오기 
+    const {product} = useContext(ProductStore);
+    
+    useEffect(() => {
+      console.log(product);
+    })
 
-  
+
   // 상대를 두개 만들기 / 펜일때 와 펜슬일때를담아준다. 
   // set상태를 페이지탑에게 넘겨준다. 
   // 상태의 값이 펜일경우 펜의 데이터를 불러와 , 컨텐트로 전달하며, 
